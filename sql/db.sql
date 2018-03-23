2605578f7cc8b0fd3f5e52dd789da21079f4c5a2
zhoubin <aa@163.com>
2018-03-23 17:40:22 +0800

    修改表结构和数据

--- sql/db.sql
+++ sql/db.sql
@@ -1,32 +1,104 @@
 /*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50712
-Source Host           : localhost:3306
-Source Database       : test
-
-Target Server Type    : MYSQL
-Target Server Version : 50712
-File Encoding         : 65001
-
-Date: 2018-01-20 16:22:55
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 5.7.12-log : Database - test
+*********************************************************************
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+/*!40101 SET NAMES utf8 */;
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `sys_menu` */
+
+DROP TABLE IF EXISTS `sys_menu`;
+
+CREATE TABLE `sys_menu` (
+  `id` varchar(64) NOT NULL COMMENT '编号',
+  `parent_id` varchar(64) DEFAULT NULL COMMENT '父级编号',
+  `parent_ids` varchar(2000) DEFAULT NULL COMMENT '所有父级编号',
+  `name` varchar(100) DEFAULT NULL COMMENT '名称',
+  `sort` decimal(10,0) DEFAULT NULL COMMENT '排序',
+  `href` varchar(2000) DEFAULT NULL COMMENT '链接',
+  `target` varchar(20) DEFAULT NULL COMMENT '目标',
+  `icon` varchar(100) DEFAULT NULL COMMENT '图标',
+  `is_show` char(1) DEFAULT NULL COMMENT '是否在菜单中显示(0显示，1隐藏)',
+  `permission` varchar(200) DEFAULT NULL COMMENT '权限标识',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT NULL COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_menu` */
+
+insert  into `sys_menu`(`id`,`parent_id`,`parent_ids`,`name`,`sort`,`href`,`target`,`icon`,`is_show`,`permission`,`create_by`,`create_date`,`update_by`,`update_date`,`remarks`,`del_flag`) values
+('1',NULL,NULL,'用户管理',10,NULL,NULL,NULL,'0',NULL,'1','2018-03-23 00:00:00','1','2018-03-23 00:00:00',NULL,'0'),
+('2',NULL,NULL,'角色管理',20,NULL,NULL,NULL,'0',NULL,'1','2018-03-23 00:00:00','1','2018-03-23 00:00:00',NULL,'0'),
+('3',NULL,NULL,'菜单管理',30,NULL,NULL,NULL,'0',NULL,'1','2018-03-23 00:00:00','1','2018-03-23 00:00:00',NULL,'0');
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` varchar(64) NOT NULL COMMENT 'id主键',
+  `name` varchar(100) DEFAULT NULL COMMENT '角色名称',
+  `enname` varchar(255) DEFAULT NULL COMMENT '英文名称',
+  `role_type` varchar(255) DEFAULT NULL COMMENT '角色类型',
+  `useable` varchar(64) DEFAULT NULL COMMENT '是否可用',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT NULL COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`id`,`name`,`enname`,`role_type`,`useable`,`create_by`,`create_date`,`update_by`,`update_date`,`remarks`,`del_flag`) values
+('1','超级管理员','admin',NULL,'1','1','2018-03-23 00:00:00','1','2018-03-23 00:00:00',NULL,'0'),
+('2','用户中心','user',NULL,'1','1','2018-03-23 00:00:00','1','2018-03-23 00:00:00',NULL,'0');
+
+/*Table structure for table `sys_role_menu` */
+
+DROP TABLE IF EXISTS `sys_role_menu`;
+
+CREATE TABLE `sys_role_menu` (
+  `role_id` varchar(64) NOT NULL COMMENT '角色id',
+  `menu_id` varchar(64) NOT NULL COMMENT '菜单id',
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role_menu` */
+
+insert  into `sys_role_menu`(`role_id`,`menu_id`) values
+('1','1'),
+('1','2'),
+('1','3'),
+('2','1');
+
+/*Table structure for table `sys_user` */
+
 DROP TABLE IF EXISTS `sys_user`;
+
 CREATE TABLE `sys_user` (
   `id` varchar(64) NOT NULL COMMENT 'id主键',
-  `login_name` varchar(100) NOT NULL COMMENT '登录名',
-  `password` varchar(100) NOT NULL COMMENT '密码',
-  `name` varchar(100) DEFAULT NULL COMMENT '姓名',
-  `email` varchar(200) COMMENT '邮箱',
-  `phone` varchar(200) COMMENT '电话',
-  `mobile` varchar(200) COMMENT '手机',
+  `login_name` varchar(20) NOT NULL COMMENT '登录名',
+  `password` varchar(64) NOT NULL COMMENT '密码',
+  `name` varchar(20) DEFAULT NULL COMMENT '姓名',
   `login_flag` char(1) DEFAULT NULL COMMENT '登录标识(0 正常, 1 禁止登录)',
   `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
   `create_date` datetime DEFAULT NULL COMMENT '创建时间',
@@ -37,68 +109,31 @@ CREATE TABLE `sys_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'zhoubin', '74dfcefa8cf9a5fd7eae4552cc1b47546ee7b9b74fb3c78a71eb061a', null, '0', null, null, null, null, null, '0');
-INSERT INTO `sys_user` VALUES ('2', 'zhangsan', '7c04e848ae0659ea503f16039ecdc4fb1eea895a1cb3a0162bc12fc4', '张三', '1', null, null, null, null, null, '0');
-INSERT INTO `sys_user` VALUES ('3', 'lisi', '96aaabcd7900f80edb79b77c01fba62b6ceb0a3e69823ea40e5315b0', '李四', '0', null, null, null, null, null, '0');
-INSERT INTO `sys_user` VALUES ('4', 'wangwu', 'f4a068c8df644fa23614d51b1c1bd22fe0951ba64d1045e54270da2f', '张三', '0', null, null, null, null, null, '0');
+/*Data for the table `sys_user` */
 
+insert  into `sys_user`(`id`,`login_name`,`password`,`name`,`login_flag`,`create_by`,`create_date`,`update_by`,`update_date`,`remarks`,`del_flag`) values
+('1','zhoubin','74dfcefa8cf9a5fd7eae4552cc1b47546ee7b9b74fb3c78a71eb061a',NULL,'0',NULL,NULL,NULL,NULL,NULL,'0'),
+('2','zhangsan','7c04e848ae0659ea503f16039ecdc4fb1eea895a1cb3a0162bc12fc4','张三','0',NULL,NULL,NULL,NULL,NULL,'0'),
+('3','lisi','96aaabcd7900f80edb79b77c01fba62b6ceb0a3e69823ea40e5315b0','李四','0',NULL,NULL,NULL,NULL,NULL,'0'),
+('4','wangwu','f4a068c8df644fa23614d51b1c1bd22fe0951ba64d1045e54270da2f','张三','0',NULL,NULL,NULL,NULL,NULL,'0');
 
-DROP TABLE IF EXISTS `sys_role`;
-create table sys_role(
-	id varchar(64) primary key not null comment 'id主键',
-	name varchar(100) comment '角色名称',
-	enname varchar(255) comment '英文名称',
-	role_type varchar(255) comment '角色类型',
-	useable varchar(64) comment '是否可用',
-	create_by varchar(64) comment '创建者',
-	create_date datetime comment '创建时间',
-	update_by varchar(64) comment '更新者',
-	update_date datetime comment '更新时间',
-	remarks varchar(255) comment '备注信息',
-	del_flag char(1) comment '删除标记'
-);
-
-
+/*Table structure for table `sys_user_role` */
 
 DROP TABLE IF EXISTS `sys_user_role`;
-create table sys_user_role(
-	user_id varchar(64) comment '用户id',
-	role_id varchar(64) comment '角色id',
-	PRIMARY KEY (user_id, role_id)
-);
 
+CREATE TABLE `sys_user_role` (
+  `user_id` varchar(64) NOT NULL COMMENT '用户id',
+  `role_id` varchar(64) NOT NULL COMMENT '角色id',
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*Data for the table `sys_user_role` */
 
-DROP TABLE IF EXISTS `sys_menu`;
-create table sys_menu(
-	id varchar(64) comment '编号',
-	parent_id varchar(64) comment '父级编号',
-	parent_ids varchar(2000) comment '所有父级编号',
-	name varchar(100) comment '名称',
-	sort decimal(10,0) comment '排序',
-	href varchar(2000) comment '链接',
-	target varchar(20) comment '目标',
-	icon varchar(100) comment '图标',
-	is_show char(1) comment '是否在菜单中显示',
-	permission varchar(200) comment '权限标识',
-	create_by varchar(64) comment '创建者',
-	create_date datetime comment '创建时间',
-	update_by varchar(64) comment '更新者',
-	update_date datetime comment '更新时间',
-	remarks varchar(255) comment '备注信息',
-	del_flag char(1) comment '删除标记',
-	primary key (id)
-)
+insert  into `sys_user_role`(`user_id`,`role_id`) values
+('1','1'),
+('2','2');
 
-
-
-create table sys_role_menu(
-        role_id varchar(64) comment '角色id',
-        menu_id varchar(64) comment '菜单id',
-        primary key (role_id, menu_id)
-);
-
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
